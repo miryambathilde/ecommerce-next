0b46a5f25a08eea0c489a15133cc1543ec6561a0
Miryam Bathilde <mbathilde@gmail.com>
2022-06-22 12:23:27 +0200

    Feature/connect sanity to app (#3)
    * added some changes and styles to banner
    
    * sanity connected to my app

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
 
+import { client } from '../lib/client';
+
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home=() => {
+const Home = ({ products, bannerData }) => {
   return (
     <div>
-      <HeroBanner/>
-
+      <HeroBanner HeroBanner={ bannerData.length && bannerData[0] } />
       <div className="products-heading">
         <h2>Best selling products</h2>
         <p>Great variety of speakers</p>
       </div>
 
       <div className="products-container">
-        { ['Product 1', 'Product 2'].map((product) => product) }
+        { products?.map((product) => product.name) }
       </div>
 
-      <FooterBanner/>
+      <FooterBanner />
     </div>
   );
+};
+
+/**
+ * https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+ * https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props
+ */
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  };
 };
 
 export default Home;
